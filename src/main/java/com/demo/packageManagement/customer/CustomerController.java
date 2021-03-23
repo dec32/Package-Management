d6aa748138f63ba36843c0194d0d9fe0f1a69f0d
dec32 <56337978+dec32@users.noreply.github.com>
2021-03-23 14:27:27 +0800

    实现了 customer 的增删改查

--- src/main/java/com/demo/packageManagement/customer/CustomerController.java
+++ src/main/java/com/demo/packageManagement/customer/CustomerController.java
@@ -3,16 +3,43 @@ package com.demo.packageManagement.customer;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
 	@Autowired
 	private CustomerService customerService;
+	//查询
 	@RequestMapping("/customers")
-	public List<Customer> getAll() {
-		return customerService.getAll();
+	public List<Customer> findAll() {
+		return customerService.findAll();
+	}
+	@RequestMapping("/customers/{id}")
+	public Customer findOne(@PathVariable("id") String id) {
+		return customerService.findOne(id);
+	}
+	
+	//增加
+	@RequestMapping(method = RequestMethod.POST, value = "/customers")
+	public void add(@RequestBody Customer customer) {
+		customerService.add(customer);
+	}
+	
+	
+	//删除
+	@RequestMapping(method = RequestMethod.DELETE, value = "customers/{id}")
+	public void delete(@PathVariable("id") String id) {
+		customerService.delete(id);
+	}
+	
+	//修改
+	@RequestMapping(method = RequestMethod.POST, value = "customers/{id}")
+	public void update(@PathVariable String id, @RequestBody Customer customer) {
+		customerService.update(id, customer);
 	}
 	
 
--- src/main/java/com/demo/packageManagement/customer/CustomerService.java
+++ src/main/java/com/demo/packageManagement/customer/CustomerService.java
@@ -12,9 +12,29 @@ import org.springframework.stereotype.Service;
 public class CustomerService {
 	@Autowired
 	private CustomerDao customerDao;
-	public List<Customer> getAll(){
+	public List<Customer> findAll(){
 		List customerList = new ArrayList<Customer>();
 		customerDao.findAll().forEach(customerList::add);
 		return customerList;
+	}
+	
+	public Customer findOne(String id) {
+		return customerDao.findOne(id);
+	}
+	
+	public void delete(String id) {
+		customerDao.delete(id);
+	}
+	
+	public void add(Customer customer) {
+		customerDao.save(customer);
+	}
+	
+	public void update(String id, Customer customer) {
+		//当传入的新对象的 id 与原有对象的 id 不一致时，应当拒绝修改
+		if(!id.equals(customer.getId())) {
+			return;
+		}
+		customerDao.save(customer);
 	}
 }
