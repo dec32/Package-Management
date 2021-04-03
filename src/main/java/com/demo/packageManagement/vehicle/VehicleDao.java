e05eb9a877ded17fac6d6c505c7ab4e76112b494
csn <csn@DESKTOP-CNHKKRG>
2021-04-03 13:52:55 +0800

    添加了warehouse表和vehicle表

--- src/main/java/com/demo/packageManagement/vehicle/VehicleDao.java
+++ src/main/java/com/demo/packageManagement/vehicle/VehicleDao.java
@@ -4,7 +4,7 @@ import javax.transaction.Transactional;
 
 import org.springframework.data.repository.CrudRepository;
 
-//数据持久层  
+//数据持久层  z
 @Transactional
 public interface VehicleDao extends CrudRepository<Vehicle, String> { 
 
